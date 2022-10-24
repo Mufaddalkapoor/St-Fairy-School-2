094180a0e8af9f2ad08777be58b6d4bd405601fb
Mufaddalkapoor <105203700+Mufaddalkapoor@users.noreply.github.com>
2022-10-24 13:52:28 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB4TdGxCFpLE7JFLHrhGpGqcNFYIbyPt94",
+    authDomain: "st-fairy-school-2.firebaseapp.com",
+    projectId: "st-fairy-school-2",
+    storageBucket: "st-fairy-school-2.appspot.com",
+    messagingSenderId: "677317756900",
+    appId: "1:677317756900:web:5fb39fd4e76b1dfba629f1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
